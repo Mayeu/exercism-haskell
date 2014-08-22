bebcbb4428194246dc445df4d20ae7e2b89bec47
Mayeu <m@6x9.fr>
2014-08-22 22:52:21 +0200

    Third iteration for leap

--- leap/LeapYear.hs
+++ leap/LeapYear.hs
@@ -1,17 +1,6 @@
 module LeapYear
 ( isLeapYear ) where
 
--- isLeapYear, test if a year is a leap year
---
--- The idea is to `mods` the year with the list [4,100,400], and depending on
--- the pattern returned determine if the year is a leap one or not.
---
--- The rule is that a year is a leap year if its divisible by 4, but not by 100
--- unless it is also divisible by 400.
-
 isLeapYear :: Int -> Bool
-isLeapYear y = case (yearMods y) of (0:0:0:[]) -> True
-                                    (0:0:xs)   -> False
-                                    (0:xs)     -> True
-                                    _          -> False
-                where yearMods y = zipWith mod (repeat y) [4,100,400]
+isLeapYear y = 4 `isADivOf` y && ( not (100 `isADivOf` y) || 400 `isADivOf` y)
+    where isADivOf x y = y `mod` x == 0
