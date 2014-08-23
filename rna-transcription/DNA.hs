0495f1b64807c84ead43c70534e0a565ef448cfe
Mayeu <m@6x9.fr>
2014-08-23 23:30:25 +0200

    Second iteration I found better

--- rna-transcription/DNA.hs
+++ rna-transcription/DNA.hs
@@ -1,8 +1,14 @@
 module DNA (toRNA) where
 
+complement :: [(Char, Char)]
+complement =
+    [ ('G', 'C')
+    , ('C', 'G')
+    , ('T', 'A')
+    , ('A', 'U')]
+
 toRNA :: String -> String
-toRNA = foldl transcribe []
-    where transcribe acc 'G' = acc ++ "C"
-          transcribe acc 'C' = acc ++ "G"
-          transcribe acc 'T' = acc ++ "A"
-          transcribe acc 'A' = acc ++ "U"
+toRNA = foldr tr []
+    where tr nucleo acc = case lookup nucleo complement of
+                               Just n  -> n:acc
+                               Nothing -> error $ "Input error: no complement for " ++ show nucleo
