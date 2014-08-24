2b1454f49c2f68da2dc5e69780ba17dce490d4dd
Mayeu <m@6x9.fr>
2014-08-24 22:46:54 +0200

    Third iteration for RNA transcription

--- rna-transcription/DNA.hs
+++ rna-transcription/DNA.hs
@@ -1,14 +1,10 @@
 module DNA (toRNA) where
 
-complement :: [(Char, Char)]
-complement =
-    [ ('G', 'C')
-    , ('C', 'G')
-    , ('T', 'A')
-    , ('A', 'U')]
-
 toRNA :: String -> String
-toRNA = foldr tr []
-    where tr nucleo acc = case lookup nucleo complement of
-                               Just n  -> n:acc
-                               Nothing -> error $ "Input error: no complement for " ++ show nucleo
+toRNA = map tr
+    where tr nucleo = case nucleo of
+                           'G' -> 'C'
+                           'C' -> 'G'
+                           'T' -> 'A'
+                           'A' -> 'U'
+                           _   -> error $ "Input error: no complement for " ++ show nucleo
