1e855339f06932754b73435fc832c2e9f1648606
Mayeu <m@6x9.fr>
2014-08-25 22:01:08 +0200

    Fourth iteration for RNA transcription

--- rna-transcription/DNA.hs
+++ rna-transcription/DNA.hs
@@ -2,9 +2,10 @@ module DNA (toRNA) where
 
 toRNA :: String -> String
 toRNA = map tr
-    where tr nucleo = case nucleo of
-                           'G' -> 'C'
-                           'C' -> 'G'
-                           'T' -> 'A'
-                           'A' -> 'U'
-                           _   -> error $ "Input error: no complement for " ++ show nucleo
+  where
+    tr nucleo = case nucleo of
+        'G' -> 'C'
+        'C' -> 'G'
+        'T' -> 'A'
+        'A' -> 'U'
+        _   -> error $ "Input error: no complement for " ++ show nucleo
