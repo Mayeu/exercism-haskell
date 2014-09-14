caaaf45872211d7a8b6ba64e5b48730401d3b982
Mayeu <m@6x9.fr>
2014-09-14 17:04:31 +0200

    Second iteration for Sublist

--- sublist/Sublist.hs
+++ sublist/Sublist.hs
@@ -1,30 +1,16 @@
-module Sublist ( Sublist (Unequal, Sublist, Equal, Superlist), sublist ) where
+module Sublist
+( Sublist (..)
+, sublist
+) where
 
-import Data.List as List
+import Data.List
 
 data Sublist = Unequal | Sublist | Equal | Superlist
              deriving (Show, Eq)
 
 sublist :: (Eq a) => [a] -> [a] -> Sublist
-sublist [] [] = Equal
-sublist [] _  = Sublist
-sublist _ []  = Superlist
 sublist xs ys
-  | xs == ys = Equal
-  | List.length xs > List.length ys = if ys `isPartOf` xs
-                                      then Superlist
-                                      else Unequal
-  | List.length ys > List.length xs = if xs `isPartOf` ys
-                                      then Sublist
-                                      else Unequal
-  | otherwise = Unequal
-
-isPartOf :: (Eq a) => [a] -> [a] -> Bool
-xs `isPartOf` ys
-  -- We guard against the fact that the xs list become bigger than the ys one
-  | List.length xs <= List.length ys =
-      if List.and $ map (\ (x, y) -> x == y) $ zip xs ys
-      then True
-      else xs `isPartOf` List.tail ys
-  | otherwise = False
-                  
+  | xs == ys          = Equal
+  | ys `isInfixOf` xs = Superlist
+  | xs `isInfixOf` ys = Sublist
+  | otherwise         = Unequal
