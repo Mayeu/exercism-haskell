fe02d48e0c0257137c20b1284d9d0427dff277e6
Mayeu <m@6x9.fr>
2014-09-16 22:24:42 +0200

    Second iteration for accumulate, making a use of the lazyness of Haskell

--- accumulate/Accumulate.hs
+++ accumulate/Accumulate.hs
@@ -1,10 +1,5 @@
 module Accumulate ( accumulate ) where
 
 accumulate :: (a -> b) -> [a] -> [b]
-accumulate f xs = reverse $ recursiveAccumulate f xs []
-
-recursiveAccumulate :: (a -> b) -> [a] -> [b] -> [b]
-recursiveAccumulate _ [] acc     = acc
-recursiveAccumulate f (x:xs) acc = 
-  let new_acc = f x : acc
-  in recursiveAccumulate f xs new_acc
+accumulate _ []     = []
+accumulate f (x:xs) = f x : accumulate f xs
