a752b5b24af5aa8f74e7bf9200ba2c23c0b7cb2b
Mayeu <m@6x9.fr>
2014-09-18 10:59:36 +0200

    Second iteration for grains

--- grains/Grains.hs
+++ grains/Grains.hs
@@ -3,12 +3,28 @@ module Grains
   , total
   ) where
 
+
 square :: Integer -> Integer
 square x = 2^(x-1)
 
-total :: Integer
-total = foldl (\ acc x -> square x + acc) 0 [1..64]
+-- Solution using the sum of the geometric series a + ar + ar^2 + ... + ar^(n-1)
+-- with a = 1, r = 2, n = 64
+--
+-- See: http://en.wikipedia.org/wiki/Geometric_series
 
--- Another solution for total
+total :: Integer
+total = 2^(64 :: Int) - 1
+
+-- Other solutions:
+--
+-- 1. unisg sum + map. Really readable
+--
 -- total :: Integer
 -- total = sum $ map square [1..64]
+--
+-- 2. using foldl'
+--
+-- import Data.List
+--
+-- total :: Integer
+-- total = foldl' (\ acc x -> square x + acc) 0 [1..64]
