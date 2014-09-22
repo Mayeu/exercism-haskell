f1152e5cfc188e24588320c54f1260503fc242c7
Mayeu <m@6x9.fr>
2014-09-22 22:58:43 +0200

    Add a first iteration for Bob

---
+++ bob/Bob.hs
@@ -0,0 +1,17 @@
+module Bob (responseFor) where
+
+import Data.Char (isUpper, isAlpha, isSpace)
+
+responseFor :: String -> String
+responseFor s
+    | silence   = "Fine. Be that way!"
+    | yell      = "Woah, chill out!"
+    | question  = "Sure."
+    | otherwise = "Whatever."
+  where
+    silence  = all isSpace s
+    filtered = filter isAlpha s
+    yell     = if filtered == ""
+                 then False
+                 else all isUpper filtered
+    question = last s == '?'
