324b39a16d43aea390e452c85f5e16dabbdda690
Mayeu <m@6x9.fr>
2014-09-30 18:29:14 +0200

    Second iteration for point mutation

--- point-mutations/DNA.hs
+++ point-mutations/DNA.hs
@@ -1,7 +1,5 @@
 module DNA (hammingDistance) where
 
 hammingDistance :: String -> String -> Int
-hammingDistance s1 s2 = foldr count 0 $ zip s1 s2
-  where count (a, b) acc
-          | a /= b    = acc + 1
-          | otherwise = acc
+hammingDistance s1 s2 = sum $ zipWith count s1 s2
+  where count a b = fromEnum (a /= b)
