50e7a90001cd11f40eae3228d17ffb191e585ee5
Mayeu <m@6x9.fr>
2014-10-06 22:48:21 +0200

    First iteration for anagram

---
+++ anagram/Anagram.hs
@@ -0,0 +1,13 @@
+module Anagram (anagramsFor) where
+
+import Data.List (sort)
+import Data.Char (toLower)
+
+anagramsFor :: String -> [String] -> [String]
+anagramsFor word = filter anagrams
+  where
+    lower = map toLower
+    anagrams s
+      | lower word == lower s = False
+      | (sort . lower) word == (sort . lower) s = True
+      | otherwise = False
